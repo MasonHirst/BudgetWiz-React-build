dc4da2938a27597e4af6acca87ed836e0eec28ab
Mason Hirst <mason.j.hirst@gmail.com>
2023-01-30 19:22:01 -0700

    refreshed the folder to start anew

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import './App.css'
+import './reset.css'
 import Header from './components/Header'
 import Login from './components/Login'
 
 function App() {
-   return <div className="App">
-      <Header />
-      <Login />
-   </div>
+   return (
+      <div className="App">
+         <Header />
+         <Login />
+      </div>
+   )
 }
 
 export default App
+
+
